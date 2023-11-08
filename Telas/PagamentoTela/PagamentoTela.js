3ca7985cfcca475e322e7c69fe06306934359944
jeniferocha <jenifermartinsdarocha@hotmail.com>
2023-11-08 15:18:57 -0300

    criacao tela pagamento

---
+++ Telas/PagamentoTela/PagamentoTela.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { ScrollView, Text, TextInput, TouchableOpacity, View, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import Header from "../Header/Header";
+import styles from "./PagamentoTelaStyle";
+
+export default function PagamentoTela() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Header />
+      <ScrollView>
+        <View style={styles.viewConteudo}>
+          <Text style={styles.textoH1}>CADASTRE SEU CARTÃO PARA PAGAMENTO</Text>
+
+          <View style={styles.divisor}></View>
+
+          {/* <Text style={styles.textoInput}>NUMERO DO CARTÃO</Text> */}
+          <TextInput style={styles.inputs} placeholder="NUMERO DO CARTÃO"/>
+
+          {/* <Text style={styles.textoInput}>NOME IMPRESSO NO CARTÃO</Text> */}
+          <TextInput style={styles.inputs} placeholder="NOME IMPRESSO NO CARTÃO"/>
+
+          {/* <Text style={styles.textoInput}>CPF DO TITULAR</Text> */}
+          <TextInput style={styles.inputs} placeholder="CPF DO TITULAR" />
+
+          <View style={styles.ViewPagamento}>
+            {/* <Text style={styles.textoInputPagamento}>VALIDADE</Text> */}
+            <TextInput style={styles.inputsPagamento} placeholder="VALIDADE"/>
+
+            {/* <Text style={styles.textoInputPagamento}>CVV</Text> */}
+            <TextInput style={styles.inputsPagamento} placeholder="CVV"/>
+          </View>
+
+          <TouchableOpacity style={styles.botao} title="Entrar" onPress={() => navigation.navigate("RetiradaTela")}>
+            <Text style={styles.textoBotao}>Cadastrar</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
