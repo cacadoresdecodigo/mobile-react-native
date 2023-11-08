893581b8242b2262432ad8efd03e8a3905f32791
jeniferocha <jenifermartinsdarocha@hotmail.com>
2023-11-08 15:18:05 -0300

    atualizacao Header

--- Telas/Header/Header.js
+++ Telas/Header/Header.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { View, Image } from "react-native";
+import { Image, View } from "react-native";
 
 import styles from "./HeaderStyle";
 
-
 export default function Header() {
-    return (
-        <View style={styles.viewHeader}>
-          <Image style={styles.imagem} source={require("../../assets/favicon3.png")} />
-        </View>
-    );
-  }
+  return (
+    <View style={styles.viewHeader}>
+      <Image style={styles.imagem} source={require("../../assets/favicon3.png")} />
+
+      <View style={styles.viewLoginAtivo}>
+        <Image style={styles.imagemLoginAtivo} source={require("../../assets/3-removebg-preview.png")} />
+        <Image style={styles.imagemLoginAtivo} source={require("../../assets/5-removebg-preview.png")} />
+        <Image style={styles.imagemLoginAtivo} source={require("../../assets/6-removebg-preview.png")} />
+        <Image style={styles.imagemLoginAtivo} source={require("../../assets/7-removebg-preview.png")} />
+
+        <Image style={styles.fotoAvatar} src={"https://i.pravatar.cc/300"} />
+      </View>
+    </View>
+  );
+}
--- Telas/Header/HeaderStyle.js
+++ Telas/Header/HeaderStyle.js
@@ -7,13 +7,31 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "flex-start",
     flexDirection: "row",
-    // backgroundColor: "#fff",
     padding: 12,
-    
   },
   imagem: {
     width: 52,
     height: 55,
+  },
+  viewLoginAtivo: {
+    height: 48,
+    width: "90%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 12,
+    gap: 10,
+  },
+  fotoAvatar: {
+    width: 52,
+    height: 55,
+    borderWidth: 0.5,
+    padding: 10,
+    borderRadius: 50,
+  },
+  imagemLoginAtivo: {
+    width: 32,
+    height: 35,
   },
 });
 
