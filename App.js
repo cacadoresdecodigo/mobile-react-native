119c27e9184e765b7857b1ab723a2f23eee35872
jeniferocha <jenifermartinsdarocha@hotmail.com>
2023-11-10 15:48:24 -0300

    correcao nome

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
+          name="HomeTela"
           component={HomeTela}
           options={{
             title: "Home",
